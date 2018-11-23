bb99f231eec707a8d3096d48b1fb7ed117e1502f
Branden Lee <Branden@Graces-MacBook-Air.local>
2018-11-22 20:31:27 -0800

    bulletbill

--- drawing/bulletbill.js
+++ drawing/bulletbill.js
@@ -29,12 +29,32 @@ function init() {
     ctx.fillRect(13*unit,5*unit,1*unit,1*unit);
     ctx.fillRect(15*unit,5*unit,1*unit,1*unit);
     ctx.fillRect(0,6*unit,5*unit,1*unit);
+    ctx.fillRect(7*unit,6*unit,5*unit,1*unit);
+    ctx.fillRect(15*unit,6*unit,1*unit,1*unit);
+    ctx.fillRect(0*unit,7*unit,4*unit,1*unit);
+    ctx.fillRect(7*unit,7*unit,1*unit,1*unit);
+    ctx.fillRect(10*unit,7*unit,6*unit,1*unit);
+    ctx.fillRect(0*unit,8*unit,4*unit,1*unit);
+    ctx.fillRect(11*unit,8*unit,5*unit,1*unit);
+    ctx.fillRect(0*unit,9*unit,5*unit,1*unit);
+    ctx.fillRect(0*unit,10*unit,7*unit,1*unit);
+    ctx.fillRect(11*unit,9*unit,4*unit,1*unit);
+    ctx.fillRect(0*unit,11*unit,7*unit,1*unit);
+    ctx.fillRect(10*unit,10*unit,5*unit,1*unit);
+    ctx.fillRect(7*unit,11*unit,7*unit,1*unit);
+    ctx.fillRect(0*unit,12*unit,12*unit,1*unit);
+    ctx.fillRect(0*unit,13*unit,2*unit,1*unit);
+    ctx.fillRect(4*unit,13*unit,6*unit,1*unit);
     ctx.fillStyle = "#ABAAA6";
     ctx.fillRect(2*unit,0,2*unit,1*unit);
     ctx.fillRect(0,2*unit,2*unit,1*unit);
     ctx.fillRect(4*unit,2*unit,5*unit,1*unit);
     ctx.fillRect(2*unit,3*unit,1*unit,1*unit);
     ctx.fillRect(9*unit,3*unit,1*unit,1*unit);
+    ctx.fillRect(4*unit,8*unit,2*unit,1*unit);
+    ctx.fillRect(5*unit,9*unit,3*unit,1*unit);
+    ctx.fillRect(7*unit,10*unit,3*unit,1*unit);
+    ctx.fillRect(10*unit,9*unit,1*unit,1*unit);
     drawRuler(5);
     // x = horizontal/left to right, y = vertical/top to bottom, w = left to right, h = height
 }
