9b4027d015fe2d332d3195f97363224795773e95
junishere <branden.jlee@gmail.com>
2019-01-12 14:20:45 -0800

    dddd

---
+++ drawing/sans.js
@@ -0,0 +1,15 @@
+// Variable: Sometjing that stores a value that could change over time.
+var canvas;
+var ctx;
+
+window.onload = init;
+// When the window (page) loads up, it calls the init function.
+
+// function: A grouop of tasks that could be called upon.
+function init() {
+    //Canvas in js connects with canvas in html.
+    canvas = document.getElementById("myCanvas");
+// Setting up the context to 20.
+ctx= canvas.getContext('2d');
+drawRuler(10);
+}
