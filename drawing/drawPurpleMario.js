5694a864c5f288ea3d14b37c3ae6841715862f44
junishere <branden.jlee@gmail.com>
2019-02-22 22:18:43 -0800

    g

--- drawing/drawPurpleMario.js
+++ drawing/drawPurpleMario.js
@@ -17,7 +17,8 @@ function drawPurpleMarioHead1() {
     
     ctx.fillStyle = "purple";
     
-    ctx.fillRect(3*UNIT,0,6*UNIT,1*UNIT);
+    ctx.fillRect(4*UNIT,0,6*UNIT,1*UNIT);
+    ctx.fillRect(3*UNIT,1,UNIT,11*UNIT,1*UNIT);
 }
 
 function drawPurpleMarioHead2() {
