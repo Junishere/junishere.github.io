5c1bd5de0ec32b2c1278d76bee4aad65a0c2bbbe
junishere <branden.jlee@gmail.com>
2019-02-22 22:39:13 -0800

    hhhhh

--- drawing/drawPurpleMario.js
+++ drawing/drawPurpleMario.js
@@ -17,8 +17,20 @@ function drawPurpleMarioHead1() {
     
     ctx.fillStyle = "purple";
     
-    ctx.fillRect(4*UNIT,0,6*UNIT,1*UNIT);
-    ctx.fillRect(3*UNIT,1,UNIT,11*UNIT,1*UNIT);
+    ctx.fillRect(3*UNIT,0,6*UNIT,1*UNIT);
+    ctx.fillRect(2*UNIT,1,UNIT,11*UNIT,1*UNIT);
+    
+    ctx.fillStyle = "#C66426"
+
+    ctx.fillRect(2*UNIT,2*UNIT,3*UNIt,1*UNIT);
+    ctx.fillRect(7*UNIT,2*UNIT,1*UNIt,2*UNIT);
+    ctx.fillRect(1*UNIT,3*UNIT,1*UNIt,3*UNIT);
+    ctx.fillRect(3*UNIT,3*UNIT,1*UNIt,2*UNIT);
+    ctx.fillRect(2*UNIT,5*UNIT,1*UNIt,1*UNIT);
+    ctx.fillRect(4*UNIT,4*UNIT,1*UNIt,1*UNIT);
+    ctx.fillRect(8*UNIT,4*UNIT,1*UNIt,1*UNIT);
+    ctx.fillRect(7*UNIT,5*UNIT,4*UNIt,1*UNIT);
+
 }
 
 function drawPurpleMarioHead2() {
