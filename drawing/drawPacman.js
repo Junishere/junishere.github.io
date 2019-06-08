1d07ea99ee8b7b36c3ca25b895c582801902d68b
junishere <branden.jlee@gmail.com>
2019-06-08 14:03:35 -0700

    hfhsdfs

--- drawing/drawPacman.js
+++ drawing/drawPacman.js
@@ -36,12 +36,51 @@ function drawPacman(x,y,frame) {
     
 }
 function drawPacmanDying(x,y,frame) {
-    // save
-    // translate
+    ctx.save();
+    ctx.translate(x*UNIT,y*UNIT);
+    ctx.fillStyle = "#FDF734";
     if (frame == 1) {
-
+        ctx.fillRect(5*UNIT,1*UNIT,5*UNIT,1*UNIT);
+        ctx.fillRect(3*UNIT,2*UNIT,9*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,3*UNIT,11*UNIT,2*UNIT);
+        ctx.fillRect(1*UNIT,5*UNIT,13*UNIT,5*UNIT);
+        ctx.fillRect(2*UNIT,10*UNIT,11*UNIT,2*UNIT);
+        ctx.fillRect(3*UNIT,12*UNIT,9*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,13*UNIT,5*UNIT,1*UNIT);
     } else if (frame == 2) {
-
-    } // go all the way to the last frame
-    // restore
+        ctx.fillRect(1*UNIT,3*UNIT,2*UNIT,1*UNIT);
+        ctx.fillRect(12*UNIT,3*UNIT,2*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,4*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(11*UNIT,4*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,5*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(10*UNIT,5*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,6*UNIT,5*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,6*UNIT,5*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,7*UNIT,6*UNIT,1*UNIT,);
+        ctx.fillRect(8*UNIT,7*UNIT,6*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,8*UNIT,11*UNIT,2*UNIT);
+        ctx.fillRect(3*UNIT,10*UNIT,9*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,11*UNIT,5*UNIT,1*UNIT);
+    } else if (frame == 3) {
+        ctx.fillRect(0,0,5*UNIT,3*UNIT);
+    } else if (frame == 4) {
+        ctx.fillRect(0,0,5*UNIT,4*UNIT);
+    } else if (frame == 5) {
+        ctx.fillRect(0,0,5*UNIT,5*UNIT);
+    } else if (frame == 6) {
+        ctx.fillRect(0,0,1*UNIT,5*UNIT);
+    } else if (frame == 7) {
+        ctx.fillRect(0,0,2*UNIT,5*UNIT);
+    } else if (frame == 8) {
+        ctx.fillRect(0,0,3*UNIT,5*UNIT);
+    } else if (frame == 9) {
+        ctx.fillRect(0,0,4*UNIT,5*UNIT);
+    } else if (frame == 10) {
+        ctx.fillRect(0,0,5*UNIT,5*UNIT);
+    } else if (frame == 11) {
+        ctx.fillRect(0,0,3*UNIT,3*UNIT);
+    } else if (frame == 12) {
+        ctx.fillRect(0,0,1*UNIT,1*UNIT);
+    }
+    ctx.restore();
 }
