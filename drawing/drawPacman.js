e4884d7341a77394acd9fff72913bf764c03a396
junishere <branden.jlee@gmail.com>
2019-06-14 23:58:21 -0700

    ghvg

--- drawing/drawPacman.js
+++ drawing/drawPacman.js
@@ -85,21 +85,72 @@ function drawPacmanDying(x,y,frame) {
         ctx.fillRect(5*UNIT,11*UNIT,3*UNIT,1*UNIT);
         ctx.fillRect(9*UNIT,11*UNIT,3*UNIT,1*UNIT);
     } else if (frame == 5) {
-        ctx.fillRect(0,0,5*UNIT,5*UNIT);
+        ctx.fillRect(1*UNIT,7*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(12*UNIT,7*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,8*UNIT,15*UNIT,2*UNIT);
+        ctx.fillRect(2*UNIT,10*UNIT,13*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,11*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,11*UNIT,4*UNIT,1*UNIT);
     } else if (frame == 6) {
-        ctx.fillRect(0,0,1*UNIT,5*UNIT);
+        ctx.fillRect(5*UNIT,6*UNIT,7*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,7*UNIT,15*UNIT,2*UNIT);
+        ctx.fillRect(2*UNIT,9*UNIT,13*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,10*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,10*UNIT,4*UNIT,1*UNIT);
     } else if (frame == 7) {
-        ctx.fillRect(0,0,2*UNIT,5*UNIT);
+        ctx.fillRect(7*UNIT,5*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,6*UNIT,7*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,7*UNIT,13*UNIT,1*UNIT);
+        ctx.fillRect(1*UNIT,8*UNIT,15*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,9*UNIT,6*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,9*UNIT,6*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,10*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(10*UNIT,10*UNIT,3*UNIT,1*UNIT);
     } else if (frame == 8) {
-        ctx.fillRect(0,0,3*UNIT,5*UNIT);
+        ctx.fillRect(8*UNIT,4*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(7*UNIT,5*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,6*UNIT,7*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,7*UNIT,9*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,8*UNIT,13*UNIT,1*UNIT);
+        ctx.fillRect(2*UNIT,9*UNIT,6*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,9*UNIT,6*UNIT,1*UNIT);
+        ctx.fillRect(3*UNIT,10*UNIT,4*UNIT,1*UNIT);
+        ctx.fillRect(10*UNIT,10*UNIT,4*UNIT,1*UNIT);
     } else if (frame == 9) {
-        ctx.fillRect(0,0,4*UNIT,5*UNIT);
+        ctx.fillRect(8*UNIT,4*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(7*UNIT,5*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(6*UNIT,6*UNIT,5*UNIT,2*UNIT);
+        ctx.fillRect(5*UNIT,8*UNIT,7*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,9*UNIT,9*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,10*UNIT,3*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,10*UNIT,3*UNIT,1*UNIT);
     } else if (frame == 10) {
-        ctx.fillRect(0,0,5*UNIT,5*UNIT);
+        ctx.fillRect(8*UNIT,4*UNIT,1*UNIT,2*UNIT);
+        ctx.fillRect(7*UNIT,6*UNIT,3*UNIT,3*UNIT);
+        ctx.fillRect(6*UNIT,9*UNIT,5*UNIT,1*UNIT);
+        ctx.fillRect(7*UNIT,10*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,10*UNIT,1*UNIT,1*UNIT);
     } else if (frame == 11) {
-        ctx.fillRect(0,0,3*UNIT,3*UNIT);
+        ctx.fillRect(8*UNIT,4*UNIT,1*UNIT,6*UNIT);
     } else if (frame == 12) {
-        ctx.fillRect(0,0,1*UNIT,1*UNIT);
+        ctx.fillRect(6*UNIT,1*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(10*UNIT,1*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,2*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(7*UNIT,2*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,2*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,3*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(12*UNIT,3*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(11*UNIT,4*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(3*UNIT,5*UNIT,2*UNIT,1*UNIT);
+        ctx.fillRect(12*UNIT,6*UNIT,2*UNIT,1*UNIT);
+        ctx.fillRect(5*UNIT,7*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(4*UNIT,8*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(11*UNIT,8*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(7*UNIT,9*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(9*UNIT,9*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(12*UNIT,9*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(6*UNIT,10*UNIT,1*UNIT,1*UNIT);
+        ctx.fillRect(10*UNIT,10*UNIT,1*UNIT,1*UNIT);
     }
     ctx.restore();
 }
